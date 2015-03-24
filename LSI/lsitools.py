64ee3353343d39c0f5d8c82d22d105637f6501db
Andrew Zenk <azenk@umn.edu>
2015-03-24 17:05:15 -0500

    Reference enclosure id rather than object.

--- LSI/lsitools.py
+++ LSI/lsitools.py
@@ -51,7 +51,7 @@ def main():
         for lsi in m.discover():
             for drive in lsi.drives():
                 props = dict()
-                props["_drive_enclosure"] = drive.enclosure
+                props["_drive_enclosure"] = drive.enclosure.enclosure_id
                 props["_drive_slot"] = drive.slot_number
                 props["_drive_health"] = drive.health
                 props["_drive_serial"] = drive.serial_number
